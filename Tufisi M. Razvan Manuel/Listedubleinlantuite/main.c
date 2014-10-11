bb58e8b6f26aee9cba340f06ba2c3a5f89ea8c39
razvantufisi <razvantufisi94@yahoo.ro>
2014-10-11 21:32:18 +0300

    Razvan Tufisi,30322

---
+++ Tufisi M. Razvan Manuel/Listedubleinlantuite/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+typedef struct node
+{
+    int key;
+    struct node* prev;
+    struct node* next;
+} nodeT;
+
+nodeT *first,*last;
+
+void init()
+{
+    first = 0;
+    last = 0;
+    first=(nodeT*)malloc(sizeof(nodeT));
+    last=(nodeT*)malloc(sizeof(nodeT));
+
+    first->key=6;
+    first->next=last;
+    first->prev=NULL;
+    last->key=5;
+    last->prev=first;
+    last->next=NULL;
+}
+
+void addfirst(int key)
+{
+    nodeT *newN=(nodeT*)malloc(sizeof(nodeT));
+    newN->prev=NULL;
+    first->prev=newN;
+    newN->next=first;
+    newN->key=key;
+
+    first=newN;
+
+
+}
+
+void addlast(int key)
+{
+    nodeT *newN=(nodeT*)malloc(sizeof(nodeT));
+    last->next=newN;
+    newN->prev=last;
+    newN->next=NULL;
+    newN->key=key;
+    last=last->next;
+
+}
+
+nodeT *finde(int key)
+{
+    nodeT *temp;
+    temp = first;
+    while ( temp != NULL )
+        if( (temp->key== key))
+        {
+            return temp;
+        }
+        else
+            temp =temp ->next ;
+    return NULL;
+}
+
+
+void afis()
+{
+    nodeT *temp;
+    temp=first;
+    while (temp != 0)
+    {
+        printf("%d  ",temp->key);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    init();
+    addfirst(7);
+    addlast(9);
+    afis();
+
+   nodeT *b=finde(9);
+    if(b!=NULL)
+        printf("%d",b->key);
+    else
+        printf("Value not found");
+    return 0;
+}

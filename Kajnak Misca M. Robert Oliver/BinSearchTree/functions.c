1468433c4b8953518a8d59475f2b461ba76fd17b
Hesiris <kajnak_robert@yahoo.com>
2014-11-05 23:06:40 +0200

    Now with nicer display of the tree

--- Kajnak Misca M. Robert Oliver/BinSearchTree/functions.c
+++ Kajnak Misca M. Robert Oliver/BinSearchTree/functions.c
@@ -1,7 +1,29 @@
 #include "functions.h"
+#include <limits.h>
+
+int power(int x, int n)
+{
+    int i;
+    if (n==1)
+        return x;
+    if (n<0)
+        return 1; ///it is mathematically incorrect, but will suffice for the current case
+    int y=x;
+    for (i=1;i<n;i++)
+        y*=x;
+    return y;
+}
 
 node* insertNode(node* np,int n)///with the number of elements we would use here, i highly doubt recursivity would cause stack overflow
 {
+    static int dpt=-1;
+    dpt++;
+    if (dpt>5)
+    {
+        printf("Maximum depth of %d exceeded\n",dpt-1);
+        dpt--;
+        return NULL;
+    }
     if (np==NULL)
     {
         np=(node*)malloc(sizeof(node));;
@@ -19,9 +41,10 @@ node* insertNode(node* np,int n)///with the number of elements we would use here
         else
         if (n>np->val)
         {
-            np->right=insertNode(np->right,n);
+            (np->right=insertNode(np->right,n));
         }
     }
+    dpt--;
     return np;
 }
 
@@ -135,4 +158,88 @@ void deleteNode(node* np,int n)
         return;///1. therefore terminates
     }
 
+}
+
+int findMaxDepth(node * np,int * md)///md as in maxDepth
+{
+    static int cd=-1;
+    if (np!=NULL)
+    {
+        cd++;
+        if (cd>*md)
+            *md=cd;
+        findMaxDepth(np->left,md);
+        findMaxDepth(np->right,md);
+        cd--;
+    }
+    return *md;
+}
+
+int findAllNodesThatAreOfDepthDepth(node * np, int * a, int d,int *n)
+{
+    static int cd=-1;///Current depth
+    if (a==NULL)
+    {
+        a=(int*)malloc(33*sizeof(int));
+        a[0]=INT_MAX;
+    }
+    if (np!=NULL)
+    {
+        cd++;
+        if (d==0)
+        {
+            a[*n]=np->val;
+            (*n)++;///to change n, I need a pointer to it. To increase it's value i need (*n)++;
+            a[*n]=INT_MAX;
+            cd--;
+            return *n;
+        }
+        if (cd+1==d)
+        {
+            a[*n]=(np->left==NULL?INT_MIN:np->left->val);
+            (*n)++;
+            a[*n]=(np->right==NULL?INT_MIN:np->right->val);
+            (*n)++;
+            a[*n]=INT_MAX;
+        }
+        if (cd+1<d)
+        {
+            findAllNodesThatAreOfDepthDepth(np->left,a,d,n);
+            findAllNodesThatAreOfDepthDepth(np->right,a,d,n);
+        }
+        cd--;
+    }
+
+    return *n;
+}
+
+void prettyPrint(node * np)
+{
+    int md=-1;///maxdepth
+    findMaxDepth(np,&md);
+
+    int *a=(int*)malloc(33*sizeof(int));///array to store the values in. For so few values(max 32 specified) I don't see how a linked list would be more advantageus
+    int nodeCount=0;
+
+    int i;
+    for (i=0;i<=md;i++)
+    {
+        findAllNodesThatAreOfDepthDepth(np,a,i,&nodeCount);
+        int j;
+        for (j=0;j<nodeCount;j++)
+        {
+            int k;
+            for (k=0;k<(power(2,md+2-i)-3)/(j==0?2:1);k++)
+            {
+                printf(" ");
+            }
+            if (a[j]!=INT_MIN)
+                printf("%3d",a[j]);
+            else
+                printf("   ");///3 spaces
+        }
+        a[0]=INT_MAX;
+        nodeCount=0;
+        printf("\n");
+    }
 }
--- Kajnak Misca M. Robert Oliver/BinSearchTree/main.c
+++ Kajnak Misca M. Robert Oliver/BinSearchTree/main.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-///Nevermind, I found the mistake, a stupid one at that. I will get a corrected verion up ASAP
+
+///It works correctly in debugger for until the first "show", but not in build and run. I narrowed it down to the memory allocation using the commented lines
+///In the delete the debugger stops several times at  free(np), but doesn't give any errors. I have no ideea why this is happening. Please give me at least a hint
 
 int main()
 {
@@ -14,6 +16,7 @@ int main()
     f=fopen("input.txt","r");
     if (f==NULL)
         return 0x404;
+
     while ((c!=EOF) && (c!='q'))
     {
         fscanf(f,"%c",&c);
@@ -23,20 +26,33 @@ int main()
             fscanf(f,"%d",&n);
             printf("-> i %d\n",n);///these printfs are for demonstration purposes only. They show what could have been the imput
             root=insertNode(root,n);
+            //int dpth=-1;printf(" depth= %d\n",findMaxDepth(root,&dpth));
+            printf("\n");
             break;
         case ('d'):
             fscanf(f,"%d",&n);
             printf("-> d %d\n",n);
             deleteNode(root,n);
+            printf("\n");
             break;
         case ('f'):
             fscanf(f,"%d",&n);
             printf("-> f %d\n",n);
             findNode(root,n);
+            printf("\n");
             break;
         case ('s'):
             printf("-> s\n");
-            preorder(root);
+            int maxDepth=-1;findMaxDepth(root,&maxDepth);
+            if (maxDepth<5)
+            {
+                prettyPrint(root);
+            }
+            else
+            {
+                printf("Tree base too large to print. Listing instead...\n");
+                preorder(root);
+            }
             printf("\n");
             break;
         case ('q'):

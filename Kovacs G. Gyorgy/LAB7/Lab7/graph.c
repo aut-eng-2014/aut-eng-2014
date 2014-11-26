0c021873955d1b15b30a30adf9fe6076ec430c20
Kovács György <georgesmth202@gmail.com>
2014-11-26 11:29:37 +0200

    Update graph.c

--- Kovacs G. Gyorgy/LAB7/Lab7/graph.c
+++ Kovacs G. Gyorgy/LAB7/Lab7/graph.c
@@ -1,3 +1,4 @@
+
 #include "graph.h"
 
 void clearGraph(int g[GRAPHSIZE][GRAPHSIZE]){
@@ -18,9 +19,9 @@ void inputGraph(int g[GRAPHSIZE][GRAPHSIZE]){
     int i;
     int a,b;
     for (i = 0; i < numberOfEdges; i++){
-        printf("\t%d.\n\tI = ",i);
+        printf("\t%d.\n\tNode 1: ",i);
         scanf("%d",&a);
-        printf("\tJ = ");
+        printf("\tNode 2: ");
         scanf("%d",&b);
 
         g[a][b] = 1;
@@ -41,29 +42,77 @@ void printGraphMatrix(int g[GRAPHSIZE][GRAPHSIZE]){
     }
 }
 
-void traverseDFT(int g[GRAPHSIZE][GRAPHSIZE]){
-    //TODO code eet
-}
+/*
+1  procedure DFS-iterative(G,v):
+2      let S be a stack
+3      S.push(v)
+4      while S is not empty
+5            v ← S.pop()
+6            if v is not labeled as discovered:
+7                label v as discovered
+8                for all edges from v to w in G.adjacentEdges(v) do
+9                    S.push(w)
+*/
 
-void traverseBFT(int g[GRAPHSIZE][GRAPHSIZE], int sourceR, int sourceC){
-    VisitMark mark[GRAPHSIZE][GRAPHSIZE];
-    List *Q;
-    int i,j,v,w;
+void traverseDFT(int g[GRAPHSIZE][GRAPHSIZE]){
+    int startingNode;
+    printf("\n\tDepth Fist Traversal:\n\tStarting Node: ");
+    scanf("%d",&startingNode);
+
+    VisitMark mark[GRAPHSIZE];
+    List *Stack = initStack();
+    int i,j;
 
     for (i = 0; i < GRAPHSIZE; i++){
-        for (j = 0; j < GRAPHSIZE; j++){
-            mark[i][j] = UNVISITED;
-        }
+            mark[i] = UNVISITED; // For starters, mark all nodes as UNVISITED
     }
 
-    mark[sourceR][sourceC] = VISITED;
-    NQ(Q,g[sourceR][sourceC]);
-    while (!isEmpty(Q)){
-        v = DQ(Q);
-        for (i = 0; i < GRAPHSIZE; i++){
+    push(&Stack,startingNode);
+    while (!isEmpty(&Stack)){
+        system("pause");
+        printStack(&Stack);
+        int temp = pop(&Stack);
+        printf("%d ",temp);
+        if (mark[temp] == UNVISITED){
+            mark[temp] = VISITED;
             for (j = 0; j < GRAPHSIZE; j++){
-                if ()
+                if (g[j][temp] == 1){
+                    printf("Pushing %d\n",j);
+                    push(&Stack,j);
+                    printStack(&Stack);
+                }
             }
+        }
+    }
+}
+
+void traverseBFT(int g[GRAPHSIZE][GRAPHSIZE]){
+    int startingNode;
+    printf("\n\tBreadth First Traversal:\n\tStarting Node: ");
+    scanf("%d",&startingNode);
+
+    VisitMark mark[GRAPHSIZE]; // to know which nodes to add
+    List *Q = initQueue();
+    int i,j;
+
+    for (i = 0; i < GRAPHSIZE; i++){
+            mark[i] = UNVISITED; // For starters, mark all nodes as UNVISITED
+    }
+
+    mark[startingNode] = VISITED;
+
+    NQ(&Q,startingNode);
+
+    while (!isEmpty(&Q)){
+        int temp = DQ(&Q);
+        printf("%d ",temp);
+        for (j = 0; j < GRAPHSIZE; j++){
+                if (g[j][temp] == 1){
+                    if (mark[j] == UNVISITED){
+                        mark[j] = VISITED;
+                        NQ(&Q,j);
+                    }
+                }
         }
     }
 }
